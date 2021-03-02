dd4e0314e600d19712e2134690b1b58ecc344228
Nykyta <nikita.germani.520@gmail.com>
2021-03-02 16:58:18 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // привет всем
+        // это коментари
+
+        //следующая строка выведет в консоль Hello world!
+        //System.out.println("Hello World!");
+
+        //System.out.println(); строка которая выводит элементы указанные в ("")
+
+        //метод выключение строчки или определенных элементов/* */
+        //System.out.println("2 + 2 = " + (2/* + 2*/));
+
+        // символ системной строки который переносит строку \n, \t дает отступ в строке ниже
+        //System.out.println("строка 1\n\tстрока 2\n\t\tстрока 3");
+
+        //
+        System.out.println("конец");
+
+    }
+}
